3a66170fe587c7239537189525fd706d1dd27bb6
zSAiki <leo.3398@hotmail.com>
2026-04-18 10:00:47 -0300

    Primeira função que adiciona um planeta na lista

---
+++ public/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const planets = [];
+function addPlanet(name, coordinates, situation) {
+    planets.push({
+        name,
+        coordinates,
+        situation,
+        satellites: []
+    });
+    alert(`O planeta ${name} foi registrado com sucesso.`);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
-let situacao :  "habitado" | "habitável" | "inabitavel" | "inexplorado"
-function newPlanet(){
-    
-    const newPlanet = {
-        nome: String,
-        coordenadas: [Number,Number,Number,Number],
-        situacao : String,
-        satelites: []
-    }
+type PlanetSituation =  "habitado" | "habitável" | "inabitavel" | "inexplorado"
+
+type PlanetCoordinates = [number, number, number, number]
+
+type Planet = { 
+    name: string,
+    coordinates: PlanetCoordinates,
+    situation: PlanetSituation,
+    satellites : string[]
+}
+
+const planets : Planet[] = []
+
+
+function addPlanet (name: string, coordinates: PlanetCoordinates, situation: PlanetSituation){
+    planets.push({
+        name,
+        coordinates,
+        situation,
+        satellites:[]
+    })
+    alert(`O planeta ${name} foi registrado com sucesso.`)
 }
