b756f29adff34b8a65e8457e80ee7322fe5abe45
zSAiki <leo.3398@hotmail.com>
2026-04-20 10:51:01 -0300

    Criação da função de remover Satélite e criação da função de mudar a situação do planeta.

--- public/dist/index.js
+++ public/dist/index.js
@@ -9,3 +9,47 @@ function addPlanet(name, coordinates, situation) {
     });
     alert(`O planeta ${name} foi registrado com sucesso.`);
 }
+function findPlanet(name) {
+    const planet = planets.find(planet => planet.name === name);
+    return planet ?? false;
+}
+function updateSituation(situation, planet) {
+    planet.situation = situation;
+    alert(`A situação do planeta ${planet.name} foi atualizada para ${situation}`);
+}
+function addSatellite(name, planet) {
+    planet.satellites.push(name);
+    alert(`O satélite ${name} foi adicionado ao planeta ${planet.name}`);
+}
+function removeSatellite(name, planet) {
+    planet.satellites = planet.satellites.filter(satellite => satellite !== name);
+    alert(`O satélite ${name} foi removido do planeta ${planet.name}`);
+}
+function promptValidSituation() {
+    let situation;
+    let validSituation = false;
+    while (!validSituation) {
+        const situationInput = prompt("Informe a situação do planeta:\n1 - Habitado\n2 - Habitável\n 3 - Inabitável\n4 - Inexplorado");
+        switch (situationInput) {
+            case "1":
+                situation = "habitado";
+                validSituation = true;
+                return situation;
+            case "2":
+                situation = "habitável";
+                validSituation = true;
+                return situation;
+            case "3":
+                situation = "inabitavel";
+                validSituation = true;
+                return situation;
+            case "4":
+                situation = "inexplorado";
+                validSituation = true;
+                return situation;
+            default:
+                alert('Situação inválida!');
+                break;
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 type PlanetSituation =  "habitado" | "habitável" | "inabitavel" | "inexplorado"
-
 type PlanetCoordinates = [number, number, number, number]
-
 type Planet = { 
     name: string,
     coordinates: PlanetCoordinates,
@@ -10,7 +8,6 @@ type Planet = {
 }
 
 const planets : Planet[] = []
-
 
 function addPlanet (name: string, coordinates: PlanetCoordinates, situation: PlanetSituation){
     planets.push({
@@ -21,3 +18,58 @@ function addPlanet (name: string, coordinates: PlanetCoordinates, situation: Pla
     })
     alert(`O planeta ${name} foi registrado com sucesso.`)
 }
+
+function findPlanet(name:string){
+    const planet = planets.find(planet=> planet.name === name)
+    return planet ?? false
+}
+
+function updateSituation (situation:PlanetSituation, planet:Planet){
+    planet.situation = situation
+
+    alert(`A situação do planeta ${planet.name} foi atualizada para ${situation}`)
+}
+
+function addSatellite(name: string, planet: Planet){
+    planet.satellites.push(name)
+
+    alert(`O satélite ${name} foi adicionado ao planeta ${planet.name}`)
+}
+
+function removeSatellite(name: string, planet:Planet){
+    planet.satellites = planet.satellites.filter(satellite => satellite !== name )
+
+    alert(`O satélite ${name} foi removido do planeta ${planet.name}`)
+}
+
+function promptValidSituation() {
+    let situation: PlanetSituation
+    let validSituation = false 
+
+    while(!validSituation){
+        const situationInput = prompt("Informe a situação do planeta:\n1 - Habitado\n2 - Habitável\n 3 - Inabitável\n4 - Inexplorado")
+
+        switch(situationInput){
+            case "1":
+                situation = "habitado"
+                validSituation = true
+                return situation
+                
+            case "2":
+                situation = "habitável"
+                validSituation = true
+                return situation 
+            case "3":
+                situation = "inabitavel"
+                validSituation = true
+                return situation 
+            case "4":
+                situation = "inexplorado"
+                validSituation = true
+                return situation 
+            default:
+                alert('Situação inválida!')
+                break
+            }
+        }
+    }
